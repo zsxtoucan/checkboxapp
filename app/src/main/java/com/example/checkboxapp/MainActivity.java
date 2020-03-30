bc6c1dbfb269ea6779750d15e4ed897ba6c0e520
JinYo <zsxtoucan@gmail.com>
2020-03-30 11:51:31 +0800

    50%

--- app/src/main/java/com/example/checkboxapp/MainActivity.java
+++ app/src/main/java/com/example/checkboxapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.checkboxapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+        TextView txvLike = findViewById(R.id.textView2);
+
+
+        int[]id={R.id.checkBox,R.id.checkBox2,R.id.checkBox3,R.id.checkBox4};
+
+
+        }
+
     }
 }
